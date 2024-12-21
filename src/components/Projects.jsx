c9bb1def64a56ed97b2dc4f1f535578e461b1f06
Samyk-s <samykbajr919@gmail.com>
2024-12-21 07:11:47 +0545

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,8 @@ const Projects = () => {
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
             <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              Our Blog
+              Projects
             </h2>
-            <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-              We use an agile approach to test assumptions and connect with the
-              needs of your audience early and often.
-            </p>
           </div>
           <div className="grid gap-8 lg:grid-cols-2">
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -18,9 +14,18 @@ const Projects = () => {
                 <a href="#">E-commerce Clothing Website</a>
               </h2>
               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
-              <li>Created a fully responsive e-commerce website for a fictional clothing store.</li>
-              <li>Implemented organized product sections to display clothing items, images, and descriptions effectively.</li>
-              <li>Designed an intuitive and clean layout to ensure smooth navigation and accessibility.</li>
+                <li>
+                  Created a fully responsive e-commerce website for a fictional
+                  clothing store.
+                </li>
+                <li>
+                  Implemented organized product sections to display clothing
+                  items, images, and descriptions effectively.
+                </li>
+                <li>
+                  Designed an intuitive and clean layout to ensure smooth
+                  navigation and accessibility.
+                </li>
               </p>
             </article>
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -28,13 +33,20 @@ const Projects = () => {
                 <a href="#">AI Trash Detection Website</a>
               </h2>
               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
-                <li>Developed an AI-powered web platform that classifies waste items as recyclable or non-recyclable in real-time.</li>
-                <li>Integrated a machine learning model for accurate waste identification.</li>
-                <li>Designed a user-friendly interface with educational content to promote sustainable waste management.</li>
-
+                <li>
+                  Developed an AI-powered web platform that classifies waste
+                  items as recyclable or non-recyclable in real-time.
+                </li>
+                <li>
+                  Integrated a machine learning model for accurate waste
+                  identification.
+                </li>
+                <li>
+                  Designed a user-friendly interface with educational content to
+                  promote sustainable waste management.
+                </li>
               </p>
             </article>
-           
           </div>
         </div>
       </section>
